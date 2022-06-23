1d629e86d4ad3cbc29c3586afb00f4c2ed34c405
Emily <emilydta@users.noreply.github.com>
2022-06-23 23:02:12 +1000

    Setup React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>test</p>
+      </div>
+    );
+  }
+}
+
+export default App;
