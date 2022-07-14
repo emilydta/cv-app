a73acff1d46a400dde9343e23bdf5074bdc4a1f1
Emily <emilydta@users.noreply.github.com>
2022-07-14 20:38:38 +1000

    minor change (remove unused useEffect)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import GeneralInformationForm from './components/forms/GeneralInformationForm';
 import EducationForm from './components/forms/EducationForm';
 import ExperienceForm from './components/forms/ExperienceForm';
